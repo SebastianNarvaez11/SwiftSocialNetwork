e24d333846dbeca5c81c4826d0bfeee2debd41e2
Sebastian Narvaez <sebastian.narvaez@aplicando.co>
2024-07-24 18:25:00 -0500

    Initial Commit

---
+++ Tasks/TasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  TasksApp.swift
+//  Tasks
+//
+//  Created by Sebastian on 24/07/24.
+//
+
+import SwiftUI
+
+@main
+struct TasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
